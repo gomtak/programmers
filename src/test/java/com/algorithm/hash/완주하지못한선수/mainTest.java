accdaca549e5325f4348102b686a95193a044d86
tak6831 <tak6831@gmail.com>
2022-05-31 20:36:30 +0900

    fix : list to String array

--- src/test/java/com/algorithm/hash/완주하지못한선수/mainTest.java
+++ src/test/java/com/algorithm/hash/완주하지못한선수/mainTest.java
@@ -4,33 +4,29 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class mainTest {
-    List<String> participant = new ArrayList<>();
-    List<String> completion = new ArrayList<>();
+    String[] participant;
+    String[] completion;
 
     @BeforeEach
     void setUp() {
-        participant.add("leo");
-        participant.add("kiki");
-        participant.add("eden");
-
-        completion.add("eden");
-        completion.add("kiki");
+        participant = new String[3];
+        participant[0]=("leo");
+        participant[1]=("kiki");
+        participant[2]=("eden");
+        completion = new String[2];
+        completion[0]=("eden");
+        completion[1]=("kiki");
     }
+
     @Test
-    void 완지하지못한자(){
-        String s = completion.stream()
-                        .flatMap(c->{
-                            return participant.stream().filter(p->{
-                                return !c.equals(p);
-                            });
-                        }).findFirst().toString();
-        System.out.println(s);
+    void 완주하지못한사람해쉬(){
 
     }
 }
