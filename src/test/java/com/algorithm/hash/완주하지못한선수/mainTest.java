28848bcdbee272a6e6e46945761a6b41fb259098
gomtak <tak6831@gmail.com>
2022-06-07 01:44:12 +0900

    완주하지못한 선수 완료

--- src/test/java/com/algorithm/hash/완주하지못한선수/mainTest.java
+++ src/test/java/com/algorithm/hash/완주하지못한선수/mainTest.java
@@ -11,11 +11,10 @@ class mainTest {
 
     @BeforeEach
     void setUp() {
-        participant = new String[4];
+        participant = new String[3];
         participant[0]=("leo");
         participant[1]=("kiki");
-        participant[2]=("kiki");
-        participant[3]=("eden");
+        participant[2]=("eden");
         completion = new String[2];
         completion[0]=("eden");
         completion[1]=("kiki");
@@ -25,14 +24,23 @@ class mainTest {
     void 완주하지못한사람해쉬() {
         Hashtable<String, Integer> hashtable = new Hashtable<>();
         int index = 0;
-        for (String t : participant) {
-            if(hashtable.get(t)==null){
-                hashtable.put(t, 1);
-            }else{
-                hashtable.put(t,hashtable.get(t)+1);
-            }
+        for(String t : participant) {
+            hashtable.put(t,hashtable.getOrDefault(t,0)+1);
             index++;
-            System.out.println(hashtable);
+        }
+        for(String t : completion){
+            if(hashtable.get(t)!=0){
+                hashtable.put(t,hashtable.get(t)-1);
+            }
+            if(hashtable.get(t)==0){
+                hashtable.remove(t);
+            }
+        }
+//        System.out.println(hashtable.keySet());
+        for(String t : participant){
+            if(hashtable.get(t)!=null){
+                System.out.println(t);
+            }
         }
     }
 }
