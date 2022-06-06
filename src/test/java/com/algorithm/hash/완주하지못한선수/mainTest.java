97a691ba083f4e464e21ac919c3c83c78f1d66f5
gomtak <tak6831@gmail.com>
2022-06-06 23:58:22 +0900

    ﻿feat : Add hashTable logic

--- src/test/java/com/algorithm/hash/완주하지못한선수/mainTest.java
+++ src/test/java/com/algorithm/hash/완주하지못한선수/mainTest.java
@@ -11,18 +11,28 @@ class mainTest {
 
     @BeforeEach
     void setUp() {
-        participant = new String[3];
+        participant = new String[4];
         participant[0]=("leo");
         participant[1]=("kiki");
-        participant[2]=("eden");
+        participant[2]=("kiki");
+        participant[3]=("eden");
         completion = new String[2];
         completion[0]=("eden");
         completion[1]=("kiki");
     }
 
     @Test
-    void 완주하지못한사람해쉬(){
-        participant;
-        Map<String, Integer> map = new HashMap<>();
+    void 완주하지못한사람해쉬() {
+        Hashtable<String, Integer> hashtable = new Hashtable<>();
+        int index = 0;
+        for (String t : participant) {
+            if(hashtable.get(t)==null){
+                hashtable.put(t, 1);
+            }else{
+                hashtable.put(t,hashtable.get(t)+1);
+            }
+            index++;
+            System.out.println(hashtable);
+        }
     }
 }
