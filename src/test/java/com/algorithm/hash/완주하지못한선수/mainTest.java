1385b88a8fe74f6f6ee9260b856a4ff6284c5478
gomtak <tak6831@gmail.com>
2022-06-06 23:58:22 +0900

    feat : Add hashTable logic

--- src/test/java/com/algorithm/hash/완주하지못한선수/mainTest.java
+++ src/test/java/com/algorithm/hash/완주하지못한선수/mainTest.java
@@ -3,9 +3,7 @@ package com.algorithm.hash.완주하지못한선수;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,17 +14,28 @@ class mainTest {
 
     @BeforeEach
     void setUp() {
-        participant = new String[3];
+        participant = new String[4];
         participant[0]=("leo");
         participant[1]=("kiki");
-        participant[2]=("eden");
+        participant[2]=("kiki");
+        participant[3]=("eden");
         completion = new String[2];
         completion[0]=("eden");
         completion[1]=("kiki");
     }
 
     @Test
-    void 완주하지못한사람해쉬(){
-
+    void 완주하지못한사람해쉬() {
+        Hashtable<String, Integer> hashtable = new Hashtable<>();
+        int index = 0;
+        for (String t : participant) {
+            if(hashtable.get(t)==null){
+                hashtable.put(t, 1);
+            }else{
+                hashtable.put(t,hashtable.get(t)+1);
+            }
+            index++;
+            System.out.println(hashtable);
+        }
     }
 }
