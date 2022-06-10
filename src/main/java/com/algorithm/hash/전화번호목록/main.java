d72d3cec47e148c274b0f94f643582a06e302cb0
gomtak <tak6831@gmail.com>
2022-06-10 22:27:28 +0900

    hash 전화번호목록

---
+++ src/main/java/com/algorithm/hash/전화번호목록/main.java
@@ -0,0 +1,4 @@
+package com.algorithm.hash.전화번호목록;
+
+public class main {
+}
---
+++ src/test/java/com/algorithm/hash/전화번호목록/mainTest.java
@@ -0,0 +1,7 @@
+package com.algorithm.hash.전화번호목록;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class mainTest {
+
+}
