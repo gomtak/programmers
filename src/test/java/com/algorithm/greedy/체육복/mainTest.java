d87a23f069098e05ff083c3787d097267368f105
gomtak <tak6831@gmail.com>
2022-06-11 17:26:13 +0900

    체육복 미통과 원인 파악 불가

--- src/test/java/com/algorithm/greedy/체육복/mainTest.java
+++ src/test/java/com/algorithm/greedy/체육복/mainTest.java
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class mainTest {
     int n;
@@ -15,14 +14,48 @@ class mainTest {
     @BeforeEach
     void setUp() {
         n=5;
-        lost = new int[]{2, 4};
-        reserve = new int[]{1, 3, 5};
+        lost = new int[]{2,4};
+        reserve = new int[]{1,3,5};
     }
     @Test
-    void 들을수있는학생수(){
+    void 체육수업들을수있는학생수(){
         int result = n-lost.length;
-        System.out.println(Arrays.binarySearch(lost,4));
-//        Arrays.stream(lost)
-//                .map();
+        for(int i=0;i<lost.length;i++){
+            for(int j=0;j<reserve.length;j++){
+                if(reserve[j]==lost[i]-1){
+                    result++;
+                    int index = Arrays.binarySearch(reserve,reserve[j]);
+                    reserve = removeArray(reserve,index);
+                    break;
+                }else if(reserve[j]==lost[i]){
+                    result++;
+                    int index = Arrays.binarySearch(reserve,reserve[j]);
+                    reserve = removeArray(reserve,index);
+                    break;
+                }else if(reserve[j]==lost[i]+1){
+                    result++;
+                    int index = Arrays.binarySearch(reserve,reserve[j]);
+                    reserve = removeArray(reserve,index);
+                    break;
+                };
+            }
+        }
+        System.out.println(result);
+    }
+    int[] removeArray(int[] array, int index){
+//        System.out.println("remove start");
+//        System.out.println(Arrays.toString(array));
+//        System.out.println(index);
+        int[] result = new int[array.length-1];
+        for(int i=0;i<result.length;i++){
+            if(i>=index){
+                result[i]=array[i+1];
+            }else{
+                result[i]=array[i];
+            }
+        }
+//        System.out.println(Arrays.toString(result));
+//        System.out.println("remove end");
+        return result;
     }
 }
