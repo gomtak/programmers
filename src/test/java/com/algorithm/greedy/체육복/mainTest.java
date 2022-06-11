0fd1ec7e9338dc3109c50a9690a399ad73c429db
gomtak <tak6831@gmail.com>
2022-06-11 19:17:53 +0900

    체육복 끝

--- src/test/java/com/algorithm/greedy/체육복/mainTest.java
+++ src/test/java/com/algorithm/greedy/체육복/mainTest.java
@@ -14,48 +14,34 @@ class mainTest {
     @BeforeEach
     void setUp() {
         n=5;
-        lost = new int[]{2,4};
+        lost = new int[]{2,4,5};
         reserve = new int[]{1,3,5};
     }
     @Test
-    void 체육수업들을수있는학생수(){
-        int result = n-lost.length;
-        for(int i=0;i<lost.length;i++){
-            for(int j=0;j<reserve.length;j++){
-                if(reserve[j]==lost[i]-1){
-                    result++;
-                    int index = Arrays.binarySearch(reserve,reserve[j]);
-                    reserve = removeArray(reserve,index);
-                    break;
-                }else if(reserve[j]==lost[i]){
-                    result++;
-                    int index = Arrays.binarySearch(reserve,reserve[j]);
-                    reserve = removeArray(reserve,index);
-                    break;
-                }else if(reserve[j]==lost[i]+1){
-                    result++;
-                    int index = Arrays.binarySearch(reserve,reserve[j]);
-                    reserve = removeArray(reserve,index);
-                    break;
-                };
+    void 체육수업들을수있는학생수() {
+        int answer = n;
+        int []student = new int[n];
+        Arrays.fill(student, 1);
+        for (int i : lost)
+            student[i - 1] -= 1;
+        for (int i : reserve)
+            student[i - 1] += 1;
+        for (int i = 0; i < student.length; i++) {
+            if (student[i] == 0) {
+                if (i > 0 && student[i - 1] == 2) {
+                    student[i - 1] -= 1;
+                    student[i] += 1;
+                }
+                else if (i < n - 1 && student[i + 1] == 2) {
+                    student[i + 1] -= 1;
+                    student[i] += 1;
+                }
+                else {
+                    answer--;
+                }
             }
         }
-        System.out.println(result);
-    }
-    int[] removeArray(int[] array, int index){
-//        System.out.println("remove start");
-//        System.out.println(Arrays.toString(array));
-//        System.out.println(index);
-        int[] result = new int[array.length-1];
-        for(int i=0;i<result.length;i++){
-            if(i>=index){
-                result[i]=array[i+1];
-            }else{
-                result[i]=array[i];
-            }
-        }
-//        System.out.println(Arrays.toString(result));
-//        System.out.println("remove end");
-        return result;
+        System.out.println(Arrays.toString(student));
+        System.out.println(answer);
     }
 }
