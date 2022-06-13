7ad27ba2b2143b54e2d3320d3c5ad7457cf5a0c8
gomtak <tak6831@gmail.com>
2022-06-13 23:16:40 +0900

    fix 체육복
    해시 펑션을 어떻게 만들지 고민.

--- src/test/java/com/algorithm/hash/전화번호목록/mainTest.java
+++ src/test/java/com/algorithm/hash/전화번호목록/mainTest.java
@@ -3,38 +3,39 @@ package com.algorithm.hash.전화번호목록;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.LinkedHashMap;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
 
 class mainTest {
     String[] phone_book;
+    LinkedList<Hashtable>[] list;
     @BeforeEach
     void setUp() {
-        phone_book = new String[]{"119", "97674223", "1195524421"};
+        phone_book = new String[]{"123", "456", "789"};
+        list = new LinkedList[phone_book.length];
     }
     @Test
     void 전화번호부확인(){
         System.out.println(solution(this.phone_book));
+
     }
     boolean solution(String[] phone_book){
-//        int[] hashCode = geHashCode(phone_book);
-        for(int i=0;i<phone_book.length;i++){
-            for(int j=0;j<phone_book.length;j++){
-                if(phone_book[i]!=phone_book[j]&&phone_book[i].contains(phone_book[j])) return false;
-                return true;
-            }
+        int minLength = 0;
+        for(String i : phone_book){
+            if(minLength==0||i.length()<minLength)
+                minLength = i.length();
         }
+        System.out.println(phone_book[0].substring(0,1));
+        LinkedList<String>[] linkedLists = getHashTable(phone_book,minLength);
         return true;
     }
 
-    private int[] geHashCode(String[] phone_book) {
-        int[] result = new int[phone_book.length];
-        for(String number : phone_book){
+    private LinkedList<String>[] getHashTable(String[] phone_book, int minLength) {
+        LinkedList<String>[] result = new LinkedList[phone_book.length];
+
+        for(String s: phone_book){
 
         }
+
         return result;
     }
 
